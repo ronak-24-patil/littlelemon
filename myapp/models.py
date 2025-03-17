f806e39ce75353e157311066f296da92629e9516
ronak-24-patil <ronak.patil23@pcu.edu.in>
2025-03-17 12:33:49 +0530

    updated menu databse

---
+++ myapp/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Model for Menu Categories
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+# Model for Menu Items
+class Menu(models.Model):
+    menu_item = models.CharField(max_length=100)
+    price = models.IntegerField()
+    category_id = models.ForeignKey(
+        MenuCategory, 
+        on_delete=models.PROTECT, 
+        default=None,
+        related_name='category_name'  # Optional: Makes querying more intuitive
+    )
+
+    def __str__(self):
+        return self.menu_item
